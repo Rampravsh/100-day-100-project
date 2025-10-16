85ed072fca431c7bba7cf4bd3d9f431b2a1a5d97
rampravesh <rampraveshkr4545@gmail.com>
2025-10-16 10:45:58 +0530

    refactor: clean up HTML structure and enhance JavaScript functionality for todo creation

--- todo app/index.js
+++ todo app/index.js
@@ -4,12 +4,24 @@ const input = document.querySelector("#input");
 const addBtn = document.querySelector("#addBtn");
 const date = document.querySelector("#date");
 const todo = document.querySelector(".single-todo");
+const todoBox = document.querySelector(".all-todo");
 
-
+const createTodo = (todoText) => {
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("single-todo");
+  todoDiv.innerHTML = `
+            <input type="checkbox" />
+            <span>${todoText}</span>
+            <button><i class="fa-solid fa-pen"></i></button>
+            <button><i class="fa-solid fa-trash"></i></button>
+  `;
+  return todoDiv;
+};
 
 addBtn.addEventListener("click", () => {
-  console.log((todo.children[1] = input.value));
-  console.log(todo);
+  // console.log((todo.children[1] = input.value));
+  // console.log(todo);
+  todoBox.appendChild(createTodo(input.value));
 });
 // console.log(date);
 
@@ -18,4 +30,9 @@ const todayDate = new Date().toLocaleDateString();
 
 date.innerText = todayDate;
 
-console.dir(todo.children[1]);
+// console.dir(todo.children[1]);
+
+// const h1 = document.createElement("h1");
+// h1.innerHTML = "heoo";
+
+// console.dir(todoBox);
