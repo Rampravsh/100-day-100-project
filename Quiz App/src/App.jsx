ff324f3f03944a1944aa83e6e59ecd15255b4cdd
rampravesh <rampraveshkr4545@gmail.com>
2025-10-16 18:00:54 +0530

    feat: implement Timer component and set up initial Quiz App structure

--- Quiz App/src/App.jsx
+++ Quiz App/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Timer from "./components/Timer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <h1 className="capitalize">hello world!</h1>
+      <Timer />
+    </>
+  );
 }
 
 export default App;
---
+++ Quiz App/src/components/Timer.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const Timer = () => {
+  const [leftTime, setleftTime] = useState(566);
+  const [displayTime, setdisplayTime] = useState("");
+
+  useEffect(() => {
+    if (leftTime <= 0) {
+      return;
+    }
+
+    const timerId = setInterval(() => {
+      setleftTime((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, [leftTime]);
+
+  useEffect(() => {
+    let formatedTime = `${Math.floor(leftTime / 60)
+      .toString()
+      .padStart(2, 0)}:${(leftTime % 60).toString().padStart(2, 0)}
+        `;
+    setdisplayTime(formatedTime);
+  }, [leftTime]);
+
+  return (
+    <div>
+      <span>⏳</span> Time Left : {displayTime} Sec
+    </div>
+  );
+};
+
+export default Timer;
